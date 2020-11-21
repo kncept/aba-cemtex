323152966edcaed37f5a33f5bfb686cd5f4520b9
Nicholas Krul <nicholas.krul@gmail.com>
2020-11-22 00:27:17 +1100

    Skeleton

---
+++ src/main/java/com/kncept/abacemtex/AbaCemtex.java
@@ -0,0 +1,4 @@
+package com.kncept.abacemtex;
+
+public class AbaCemtex {
+}
---
+++ src/test/java/com/kncept/abacemtex/AbaCemtexText.java
@@ -0,0 +1,10 @@
+package com.kncept.abacemtex;
+
+import org.junit.jupiter.api.Test;
+
+public class AbaCemtexText {
+
+    @Test
+    public void stubTest() {
+    }
+}
